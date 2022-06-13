7d94603a87f965e8a558ee8c06e1132801f798ad
wenlli <1142085024@qq.com>
2022-06-13 11:38:22 +0800

    Battle练习代码1

---
+++ username和password为输入错误拒绝进入系统.py
@@ -0,0 +1,14 @@
+
+#登录页路由
+from common import Common
+
+uri = '/login'
+# username变量存储用户名参数
+username = 'time.geekbang.org'
+# password变量存储密码参数
+password = 'time.geekbang.org'
+# 拼凑body的参数
+payload = 'username=' + username + '&password=' + password
+comm = Common()
+response_login = comm.post(uri,params=payload)
+print('Response内容：' + response_login.text)
